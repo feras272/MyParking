8fef845bb6df49401c91a718874ab9cdbe61a793
feras alnowiser <ferasdev@gmail.com>
2023-01-06 18:01:30 +0300

    add: Handler for waiting to register the user

---
+++ app/src/main/java/com/example/myparking/ui/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myparking.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.myparking.MainActivity
+import com.example.myparking.R
+import com.example.myparking.Singup
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            //finishedAnimation = false
+            //if(user != null) {
+
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i)
+                finish()
+
+            //} else {
+                val intent = Intent(this, Singup::class.java)
+                startActivity(intent)
+                finish()
+            //}
+
+        }, 3000) // 3000 is the delayed time in milliseconds.
+
+    }
+}
